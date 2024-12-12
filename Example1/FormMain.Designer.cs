0a977787613f8187f084f0440999f15809a7abfe
Nanthapat22 <6510301020@cdti.ac.th>
2024-12-12 20:48:12 +0700

    Initial commit

---
+++ Example1/FormMain.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace Example1
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.c = new System.Windows.Forms.TextBox();
+            this.buttonFlipVer = new System.Windows.Forms.Button();
+            this.buttonFlipHor = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.imageBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(676, 397);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("imageBox1.BackgroundImage")));
+            this.imageBox1.ErrorImage = ((System.Drawing.Image)(resources.GetObject("imageBox1.ErrorImage")));
+            this.imageBox1.Location = new System.Drawing.Point(6, 21);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(664, 370);
+            this.imageBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Location = new System.Drawing.Point(694, 211);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(181, 80);
+            this.buttonOK.TabIndex = 2;
+            this.buttonOK.Text = "OK";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.c);
+            this.groupBox2.Controls.Add(this.buttonFlipVer);
+            this.groupBox2.Controls.Add(this.buttonFlipHor);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 432);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(782, 167);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(676, 145);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 7;
+            // 
+            // c
+            // 
+            this.c.Location = new System.Drawing.Point(570, 145);
+            this.c.Name = "c";
+            this.c.Size = new System.Drawing.Size(100, 22);
+            this.c.TabIndex = 6;
+            // 
+            // buttonFlipVer
+            // 
+            this.buttonFlipVer.Location = new System.Drawing.Point(650, 68);
+            this.buttonFlipVer.Name = "buttonFlipVer";
+            this.buttonFlipVer.Size = new System.Drawing.Size(75, 23);
+            this.buttonFlipVer.TabIndex = 5;
+            this.buttonFlipVer.Text = "FlipVer";
+            this.buttonFlipVer.UseVisualStyleBackColor = true;
+            this.buttonFlipVer.Click += new System.EventHandler(this.buttonFlipVer_Click);
+            // 
+            // buttonFlipHor
+            // 
+            this.buttonFlipHor.Location = new System.Drawing.Point(538, 68);
+            this.buttonFlipHor.Name = "buttonFlipHor";
+            this.buttonFlipHor.Size = new System.Drawing.Size(75, 23);
+            this.buttonFlipHor.TabIndex = 4;
+            this.buttonFlipHor.Text = "FlipHor";
+            this.buttonFlipHor.UseVisualStyleBackColor = true;
+            this.buttonFlipHor.Click += new System.EventHandler(this.buttonFlipHor_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(390, 68);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(97, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Disconnect";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(284, 68);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Pause";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(167, 68);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(46, 69);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(46, 111);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(106, 31);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "off camera";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(963, 690);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.buttonOK);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "FormMain";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBox1;
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button buttonFlipHor;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonFlipVer;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox c;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Example1/FormMain.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Example1
+{
+    public partial class FormMain : Form
+    {
+        private VideoCapture _capture = null;
+        private Mat _frame;
+        private void ProcessFrame(object sender, EventArgs e)
+        {
+            if (_capture != null && _capture.Ptr != IntPtr.Zero)
+            {
+                bool canCapture = _capture.Retrieve(_frame, 0);
+                if (canCapture)
+                {
+                    imageBox1.Image = _frame;
+                }
+            }
+          
+        }
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+             
+                if (_capture != null)
+                {
+                    _capture.Dispose();
+                }
+
+                _capture = new VideoCapture(0);
+
+                // ตรวจสอบการเชื่อมต่อกับกล้อง
+                if (!_capture.IsOpened)
+                {
+                    MessageBox.Show("Cannot access the camera. Please check your device.");
+
+                    // เปลี่ยนพื้นหลังเป็นสีแดง (แค่สี ไม่มีข้อความ)
+                    textBox2.BackColor = Color.Red;
+                }
+                else
+                {
+                    // เชื่อมต่อกล้องได้
+                    _capture.ImageGrabbed += ProcessFrame; // เชื่อมต่อกับ event ที่ประมวลผลเฟรม
+                    _frame = new Mat(); // สร้าง Mat สำหรับเก็บภาพจากกล้อง
+
+                    // เริ่มต้นการจับภาพ
+                    _capture.Start();
+
+                    // แสดงข้อความ "Connected" และสีพื้นหลังเป็นเขียว
+                    c.BackColor = Color.Green;
+                    c.Text = "Connected";
+                }
+            }
+            catch (Exception ex)
+            {
+                // แสดงข้อผิดพลาดหากมี
+                MessageBox.Show($"Error: {ex.Message}");
+
+                // เปลี่ยนพื้นหลังเป็นสีแดง (แค่สี ไม่มีข้อความ)
+                textBox2.BackColor = Color.Red;
+            }
+        }
+
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (_capture != null) 
+            {
+                _capture.Start();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // ตรวจสอบว่า _capture กล้องเปิดอยู่หรือไม่
+                if (_capture != null && _capture.IsOpened)
+                {
+                    // หยุดการจับภาพจากกล้อง
+                    _capture.Stop(); // หยุดการจับภาพ
+                    _capture.Dispose(); // ปล่อยทรัพยากร
+                    _capture = null; // รีเซ็ต _capture
+
+                    // ล้างภาพใน imageBox1
+                    imageBox1.Image = null;
+
+                    // เปลี่ยนข้อความใน TextBox และปุ่ม
+                    textBox2.BackColor = Color.Red; // พื้นหลังเป็นสีแดง
+                    textBox2.Text = "Disconnected"; // แสดงข้อความ "Disconnected"
+
+                    c.BackColor = Color.Gray; // พื้นหลังปุ่มเป็นสีเทา
+                    c.Text = "Not Connected"; // แสดงข้อความ "Not Connected"
+                }
+                else
+                {
+                    // แจ้งเตือนหากกล้องไม่ได้เชื่อมต่อ
+                    MessageBox.Show("Camera is not connected.");
+                }
+            }
+            catch (Exception ex)
+            {
+                // แสดงข้อผิดพลาด
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            if (_capture != null)
+            {
+                _capture.Pause();
+            }
+        }
+
+        private void buttonFlipHor_Click(object sender, EventArgs e)
+        {
+            if (_capture != null)
+                _capture.FlipHorizontal = !_capture.FlipHorizontal;
+        }
+
+        private void buttonFlipVer_Click(object sender, EventArgs e)
+        {
+            if (_capture != null)
+                _capture.FlipVertical = !_capture.FlipVertical;
+        }
+
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+
+
+    }
+}
